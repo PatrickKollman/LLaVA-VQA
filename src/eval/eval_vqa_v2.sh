1be0444383a3b7a378bd6470cce34acb2e675135
Patrick Kollman <pkollman@andrew.cmu.edu>
2024-06-29 19:23:51 -0600

    fix: tweak eval script

--- src/eval/eval_vqa_v2.sh
+++ src/eval/eval_vqa_v2.sh
@@ -7,17 +7,18 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 
 CHUNKS=${#GPULIST[@]}
 
+# Python Script
 VQA_LOADER="eval.eval_vqa_v2"
 
+# Model
 MODEL_PATH="liuhaotian/llava-v1.5-13b"
 CKPT="llava-v1.5-13b"
 CONV_MODE="vicuna_v1"
 
+# Data Paths
+IMAGE_FOLDER="/content/drive/MyDrive/VQA/data/Images/test2015"
 QUESTION_FILE="/content/v2_OpenEnded_mscoco_test-dev2015_questions.json"
-IMAGE_FOLDER="/content/test2015"
-
 ANSWERS_DIR="/content/answers"
-OUTPUT_FILE="$ANSWERS_DIR/merge.json"
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m $VQA_LOADER \
@@ -34,6 +35,7 @@ done
 wait
 
 # Clear out the output file if it exists.
+OUTPUT_FILE="$ANSWERS_DIR/merge.json"
 > $OUTPUT_FILE
 
 # Loop through the indices and concatenate each file.
