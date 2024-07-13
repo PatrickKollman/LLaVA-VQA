47dfc6bd808058d4943c8203824d41d3304c048e
Patrick Kollman <pkollman@andrew.cmu.edu>
2024-07-13 11:16:39 -0600

    fix: update training script

--- src/train/finetune_vizwiz_lora.sh
+++ src/train/finetune_vizwiz_lora.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Global Batch Size = 128 = per_device_train_batch_size * num_gpus * gradient_accumulation_steps
+
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path /content/train.json \
+    --validation_data_path /content/val.json
+    --image_folder /content/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -15,7 +18,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora \
+    --output_dir /content/checkpoints \
     --num_train_epochs 1 \
     --per_device_train_batch_size 128 \
     --per_device_eval_batch_size 4 \
