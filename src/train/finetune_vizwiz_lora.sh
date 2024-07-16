5f77fb4fb40b9e28a91f43149951e2cf5c4e1e10
Patrick Kollman <pkollman@andrew.cmu.edu>
2024-07-16 13:31:45 -0600

    fix: checkpoint dir

--- src/train/finetune_vizwiz_lora.sh
+++ src/train/finetune_vizwiz_lora.sh
@@ -17,7 +17,7 @@ deepspeed /content/LLaVA-VQA/LLaVA/llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /content/checkpoints \
+    --output_dir /content/drive/MyDrive/VizWiz/model_checkpoints \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
